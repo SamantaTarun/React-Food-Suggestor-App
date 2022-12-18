226c69d14665093a0298737d6131e36ef92c7bba
Tarun Samanta <tarunsamanta77@gmail.com>
2022-12-18 20:31:37 +0530

    added UseEffectHook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Resturant from "./components/Basics/Resturant";
 import UseState from "./components/Hooks/useState";
+import UseEffect from "./components/Hooks/useEffect";
 
 const App = () => {
   return (
     <div>
       {/* <Resturant /> */}
-       <UseState/> 
+       {/* <UseState/>  */}
+       <UseEffect />
     </div>
   );
 };
--- src/components/Hooks/useState.js
+++ src/components/Hooks/useState.js
@@ -2,14 +2,13 @@ import React, { useState } from "react";
 import "./style.css";
 
 const UseState = () => {
-  
-  const [myNum, setMyNum] =  useState(0);
+  const [myNum, setMyNum] = useState(0);
 
   return (
     <>
       <div className="center_div">
         <p>{myNum}</p>
-        <div className="button2" onClick={() => setMyNum(myNum+1)}>
+        <div className="button2" onClick={() => setMyNum(myNum + 1)}>
           <span></span>
           <span></span>
           <span></span>
@@ -17,14 +16,16 @@ const UseState = () => {
           INCR
         </div>
 
-        <div className="button2" onClick={() => (myNum >0 ?setMyNum(myNum-1): setMyNum(0))}>
+        <div
+          className="button2"
+          onClick={() => (myNum > 0 ? setMyNum(myNum - 1) : setMyNum(0))}
+        >
           <span></span>
           <span></span>
           <span></span>
           <span></span>
           DECR
         </div>
-
       </div>
     </>
   );
